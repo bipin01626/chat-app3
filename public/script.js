f66e551c91a9c8e8be3b085b8c33deddafc75863
bipin01626 <bipinroy959@gmail.com>
2026-05-20 21:02:45 +0530

    final fix chat app

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
 const socket = io();
 
-// send message
 function sendText() {
   let input = document.getElementById("msg");
   let msg = input.value;
@@ -11,24 +10,20 @@ function sendText() {
   input.value = "";
 }
 
-// receive msg
 socket.on("chat message", (data) => {
   let li = document.createElement("li");
   li.innerText = data.user + ": " + data.message;
   document.getElementById("messages").appendChild(li);
 });
 
-// show users
 socket.on("users", (users) => {
   document.getElementById("users").innerText = users.join(", ");
 });
 
-// full alert
 socket.on("full", (msg) => {
   alert(msg);
 });
 
-// enter key
 document.getElementById("msg").addEventListener("keypress", (e) => {
   if (e.key === "Enter") sendText();
 });
--- server.js
+++ server.js
@@ -3,27 +3,31 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// static folder
 app.use(express.static("public"));
+
+// FIX: root route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 let users = [];
 
 io.on("connection", (socket) => {
 
-  // ❌ block extra users
+  // only 2 users
   if (users.length >= 2) {
     socket.emit("full", "Only 2 users allowed ❌");
     socket.disconnect();
     return;
   }
 
-  // assign names
   let userName = users.length === 0 ? "Me" : "User";
 
   users.push({ id: socket.id, name: userName });
 
   io.emit("users", users.map(u => u.name));
 
-  // message
   socket.on("chat message", (msg) => {
     io.emit("chat message", {
       user: userName,
@@ -31,7 +35,6 @@ io.on("connection", (socket) => {
     });
   });
 
-  // disconnect
   socket.on("disconnect", () => {
     users = users.filter(u => u.id !== socket.id);
     io.emit("users", users.map(u => u.name));
